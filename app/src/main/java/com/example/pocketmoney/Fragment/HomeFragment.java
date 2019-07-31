b66c2e75345bb21fa9debd84dcb163fc5f270423
thanksthanks <flzl0307@naver.com>
2019-07-31 12:06:07 +0900

    game

--- app/src/main/java/com/example/pocketmoney/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/pocketmoney/Fragment/HomeFragment.java
@@ -1,16 +1,13 @@
 package com.example.pocketmoney.Fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.pocketmoney.R;
 

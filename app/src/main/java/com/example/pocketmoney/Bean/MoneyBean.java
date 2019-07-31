4aee2cb2cf4254d62accf489bb3510096359bbb9
vimpizz <vimpizz321@gmail.com>
2019-07-31 10:28:51 +0900

    수정(도연)

--- app/src/main/java/com/example/pocketmoney/Bean/MoneyBean.java
+++ app/src/main/java/com/example/pocketmoney/Bean/MoneyBean.java
@@ -1,6 +1,9 @@
 package com.example.pocketmoney.Bean;
 
 public class MoneyBean {
-
+    public int money;
+    public long moneyId; //고유번호
+    public String source; //수입지출 종류
+    public String moneyDate;
 
 }
--- app/src/main/java/com/example/pocketmoney/Fragment/WriteFragment.java
+++ app/src/main/java/com/example/pocketmoney/Fragment/WriteFragment.java
@@ -1,6 +1,7 @@
 package com.example.pocketmoney.Fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -12,17 +13,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.pocketmoney.Activity.LoginActivity;
+import com.example.pocketmoney.Activity.MainActivity;
+import com.example.pocketmoney.Activity.WriteActivity;
 import com.example.pocketmoney.R;
 
 
 public class WriteFragment extends Fragment {
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_write,container, false);
 
+        view.findViewById(R.id.btnIncome).setOnClickListener(mBtnClick);
+        view.findViewById(R.id.btnSpend).setOnClickListener(mBtnClick);
+
         return view;
     }
 
+
+    private View.OnClickListener mBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btnIncome:
+                    Intent i = new Intent(getContext(), WriteActivity.class);
+                    startActivity(i);
+                    break;
+                case R.id.btnSpend:
+                    break;
+
+            }
+        }
+    };
 }

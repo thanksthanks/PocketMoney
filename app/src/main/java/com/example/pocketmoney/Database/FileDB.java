540b46f6ac735787ab3f91078dc954da192f3734
vimpizz <vimpizz321@gmail.com>
2019-07-31 10:44:58 +0900

    수정(도연)

---
+++ app/src/main/java/com/example/pocketmoney/Database/FileDB.java
@@ -0,0 +1,11 @@
+package com.example.pocketmoney.Database;
+
+import com.google.gson.Gson;
+
+public class FileDB {
+
+    private  static final String FILE_DB="FileDB";
+    private  static Gson mGson = new Gson();
+
+
+}

808be621dd8ed3cb434faa184107351f5c95483e
thanksthanks <flzl0307@naver.com>
2019-08-22 10:07:37 +0900

    game

---
+++ app/src/main/java/com/example/pocketmoney/Bean/GameBean.java
@@ -0,0 +1,16 @@
+package com.example.pocketmoney.Bean;
+
+public class GameBean {
+    public int coin = 150;
+
+    public boolean MissionDonation1;
+    public boolean MissionDonation2;
+    public boolean MissionInsurance1;
+    public boolean MissionInsurance2;
+    public boolean MissionInvest1;
+    public boolean MissionInvest2;
+    public boolean MissionSaving1;
+    public boolean MissionSaving2;
+    public boolean MissionSpending1;
+    public boolean MissionSpending2;
+}

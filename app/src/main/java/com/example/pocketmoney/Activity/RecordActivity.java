0a6d2ba3d80318632d5736f29b30c8b52a2f0241
vimpizz <vimpizz321@gmail.com>
2019-10-02 16:44:15 +0900

    toolbar + record activity

---
+++ app/src/main/java/com/example/pocketmoney/Activity/RecordActivity.java
@@ -0,0 +1,52 @@
+package com.example.pocketmoney.Activity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.pocketmoney.R;
+
+public class RecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+
+        Toolbar toolbar = findViewById(R.id.toolbar); //툴바설정
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.record_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+            case R.id.menu_save:{
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
